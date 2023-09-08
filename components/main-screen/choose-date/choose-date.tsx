cf4b3666d8d6b58983bfcaf800be6a9135fb58ec
Yusup <hunzah9@mail.ru>
2023-09-08 21:07:39 +0300

    commit

--- components/main-screen/choose-date/choose-date.tsx
+++ components/main-screen/choose-date/choose-date.tsx
@@ -1,60 +1,26 @@
 import React, {useState} from 'react';
-import {Image, Modal, StyleSheet, Text, TouchableOpacity, TouchableWithoutFeedback, View,} from 'react-native';
+import {Image, StyleSheet, View} from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
-export const DateSelect = ({selectedDate, onSelectDate}) => {
-    const [modalVisible, setModalVisible] = useState(false);
-
-    const handleDateSelect = (date) => {
-        onSelectDate(date);
-        setModalVisible(false);
-    };
+export const DateSelect = () => {
+    const [date, setDate] = useState(new Date());
 
     return (
         <View style={styles.root}>
-            <TouchableOpacity
-                style={styles.select}
-                onPress={() => setModalVisible(!modalVisible)}
-            >
-                <Text style={styles.text}>12.3.2023</Text>
-                <Image source={require('./../../../assets/icons/calendar.png')}/>
-            </TouchableOpacity>
-            <Modal
-                visible={modalVisible}
-                transparent={true}
-                animationType="slide"
-            >
-                <TouchableWithoutFeedback
-                    onPress={() => setModalVisible(false)}
-                >
-                    <View style={styles.modalBackground}>
-                        <View style={styles.modalContainer}>
-                            {/*<DatePickerIOS*/}
-                            {/*    style={styles.datePicker}*/}
-                            {/*    date={new Date()} // Используйте выбранную дату*/}
-                            {/*    mode="date"*/}
-                            {/*    onDateChange={handleDateSelect}*/}
-                            {/*/>*/}
-                        </View>
-                    </View>
-                </TouchableWithoutFeedback>
-            </Modal>
+            <DateTimePicker
+                themeVariant={'dark'}
+                value={new Date()}
+                mode="date"
+                textColor={'white'}
+            />
+            <Image source={require('./../../../assets/icons/calendar.png')}/>
         </View>
+
     );
 };
 
 const styles = StyleSheet.create({
     root: {
-        position: 'relative',
-    },
-    text: {
-        fontFamily: 'Dosis-Regular',
-        fontStyle: 'normal',
-        fontSize: 18,
-        fontWeight: '400',
-        letterSpacing: 0.36,
-        color: 'black',
-    },
-    select: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
@@ -64,22 +30,5 @@ const styles = StyleSheet.create({
         paddingLeft: 16,
         borderRadius: 10,
         backgroundColor: 'rgba(236,228,222,0.71)',
-        marginTop: 7,
-    },
-    modalBackground: {
-        flex: 1,
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    modalContainer: {
-        width: 300,
-        height: 400,
-        borderRadius: 10,
-        backgroundColor: '#DCCEBE',
-        alignItems: 'center',
-    },
-    datePicker: {
-        marginTop: 20,
     },
 });
